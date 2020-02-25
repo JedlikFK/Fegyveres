ef321ecb6964fc1e0aca053bf0bccdb8185186fb
Tök.Zsuzsa.Kinga <Tk.Zsuzsa.Kinga@C41-07>
2020-02-25 13:17:22 +0100

    Fegyverek tárolása.

--- fegyveres/Program.cs
+++ fegyveres/Program.cs
@@ -8,8 +8,21 @@ namespace fegyveres
 {
     class Program
     {
+        enum fegyverek {ApaÖkle, Uzi, M4, AK47, LángSzóró, KéziGránát};
         static void Main(string[] args)
         {
+            fegyverek kezemben = fegyverek.KéziGránát;
+            List<fegyverek> fegyvertár = new List<fegyverek>();
+
+            fegyvertár.Add(fegyverek.Uzi);
+            fegyvertár.Add(fegyverek.LángSzóró);
+
+            Console.WriteLine("Fegyverek a tárban: ");
+            foreach (fegyverek fegyver in fegyvertár)
+            {
+                Console.WriteLine(fegyver);
+            }
+
             Console.ReadKey();
         }
     }
